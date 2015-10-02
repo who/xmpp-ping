cc835c301f2e1677b54b1743774200e9420faa58
Andrew B <who@users.noreply.github.com>
2015-10-02 15:02:17 -0700

    Initial commit

---
+++ xmpp-ping.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var uuid = require('uuid')
+var program = require('commander')
+var Client = require('node-xmpp-client')
+var ltx = require('ltx')
+
+program
+    .option('-d, --XMPPDomain [value]', 'the XMPP domain to connect to')
+    .option('-j, --JID [value]', 'JID of user to connect with')
+    .option('-p, --password [value]', 'password of user user. defaults to "password"')
+    .option('-r, --pingRate [value]', 'ms rate at which to ping the server. defaults to 1000')
+    .option('-x, --exitOnError [value]', 'boolean to exit on error or not. defaults to true')
+    .parse(process.argv);
+
+var XMPPDomain = program.XMPPDomain;
+var JID = program.JID;
+var accountPassword = program.password;
+var pingRate = program.pingRate || 1000;
+var exitOnError = program.exitOnError || true;
+
+var client = new Client({
+    jid: JID,
+    password: accountPassword,
+    host: XMPPDomain
+})
+
+client.on('online', function() {
+    console.log('Connection successfull. Begin pinging. Ping rate (ms) : '+pingRate);
+    setInterval(function() {
+        var pingStanza = new ltx.Element('iq', {
+                from: JID + '/xmppClientTest',
+                to: XMPPDomain,
+                type: "get",
+                id: uuid()
+            })
+            .c('ping', {
+                xmlns: 'urn:xmpp:ping'
+            });
+
+        console.log('Sending ping stanza:');
+        console.log(pingStanza.root().toString());
+        client.send(pingStanza);
+    }, pingRate);
+})
+
+client.on('stanza', function(stanza) {
+    console.log("Received stanza:")
+    console.log(stanza.root().toString());
+})
+
+client.on('error', function(e) {
+    console.error(e);
+    if (exitOnError === true) {
+        console.log("Exiting process");
+        process.exit(1);
+    }
+})
